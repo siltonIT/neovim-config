ab9e2b7d45a1f5a4f90805c7f8bbde5271ad9b29
Sergei Lazaruk <silton.work@gmail.com>
2024-10-29 14:19:20 +0300

    Add icons

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -3,4 +3,21 @@ vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSig
 vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
 vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
 
-require("neo-tree").setup({})
+require("neo-tree").setup {
+    default_component_configs = {
+        indent = {
+            with_expanders = true,  -- Включает возможность сворачивания
+        },
+        icon = {
+            folder_empty = "",  -- Иконка для пустых папок
+            folder_open = "",    -- Иконка для открытых папок
+            folder_closed = "",  -- Иконка для закрытых папок
+            default = "",        -- Иконка по умолчанию для файлов
+        },
+    },
+    filesystem = {
+        filtered_items = {
+            hide_dotfiles = false,  -- Показать скрытые файлы
+        },
+    },
+}

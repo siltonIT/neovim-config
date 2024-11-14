69691ae8abc52aeea92c8af7d5bd148a5b879c65
Sergei Lazaruk <silton.work@gmail.com>
2024-11-15 00:25:30 +0300

    Add Cntrl Z

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -4,3 +4,4 @@ vim.opt.tabstop = 3
 vim.opt.shiftwidth = 3
 vim.opt.guifont = "JetBrainsMono Nerd Font:h12"
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-z>', '<C-o>u', { noremap = true, silent = true })
